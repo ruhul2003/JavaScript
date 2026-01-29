9bfe9943f8f6d05dabb99bbda67506f01d384dab
ruhul2003 <ruhul941020@gmail.com>
2026-01-30 01:29:52 +0600

    Object

---
+++ object.js
@@ -0,0 +1,18 @@
+let students=[
+    {
+        name:"Ruhul Amin",
+        ID : "41230301323",
+        Dept : "CSE",
+        Mark : "89"
+    },
+    {
+        name:"Rakib",
+        ID : "41437301323",
+        Dept : "CSE",
+        Mark : "95"
+    }
+]
+
+for(let student of students){
+    console.log(student.name,":",student.Mark);
+}
