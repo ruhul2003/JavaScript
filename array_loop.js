225a566a9af18ad27a16b748071fefee42049e48
ruhul2003 <ruhul941020@gmail.com>
2026-01-29 21:55:14 +0600

    Reversed and pushed in another array

--- array_loop.js
+++ array_loop.js
@@ -1,12 +1,23 @@
 let numbers = [10, 20, 30, 40, 50];
 console.log("Original array:", numbers);
 
-for(let i=0; i<numbers.length;i++){
-    console.log(numbers[i])
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
 }
 
-// reverse
-
-for(i=numbers.length;i>=0;i--){
-    console.log("Reversed Array :",numbers[i])
+// reverse numbers
+for (let i = numbers.length - 1; i >= 0; i--) {
+    console.log("Reversed Array:", numbers[i]);
 }
+
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+console.log(colors);
+
+let reversed = [];
+
+for (let i = colors.length - 1; i >= 0; i--) {
+    reversed.push(colors[i]);
+}
+
+console.log("Reversed colors:", reversed);
+console.log(reversed.join('+'));
