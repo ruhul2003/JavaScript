85c421b5af7077218f325878e8d2226da03f6277
ruhul2003 <ruhul941020@gmail.com>
2026-01-29 21:30:16 +0600

    Array reversal with for loop

---
+++ array_loop.js
@@ -0,0 +1,12 @@
+let numbers = [10, 20, 30, 40, 50];
+console.log("Original array:", numbers);
+
+for(let i=0; i<numbers.length;i++){
+    console.log(numbers[i])
+}
+
+// reverse
+
+for(i=numbers.length;i>=0;i--){
+    console.log("Reversed Array :",numbers[i])
+}
